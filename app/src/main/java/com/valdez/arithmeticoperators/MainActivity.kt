6a46e98088cf96a238129fe0075ccd7065a168ee
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-04-21 11:05:44 +0800

    feat: declare variables and find their views

--- app/src/main/java/com/valdez/arithmeticoperators/MainActivity.kt
+++ app/src/main/java/com/valdez/arithmeticoperators/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.valdez.arithmeticoperators
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
+    private lateinit var etFirstNumber: EditText
+    private lateinit var etSecondNumber: EditText
+    private lateinit var tvResult: TextView
+    private lateinit var btnAdd: Button
+    private lateinit var btnSubtract: Button
+    private lateinit var btnMultiply: Button
+    private lateinit var btnDivide: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        etFirstNumber = findViewById(R.id.et_first_number)
+        etSecondNumber = findViewById(R.id.et_second_number)
+        tvResult = findViewById(R.id.tv_result)
+        btnAdd = findViewById(R.id.btn_add)
+        btnSubtract = findViewById(R.id.btn_subtract)
+        btnMultiply = findViewById(R.id.btn_multiply)
+        btnDivide = findViewById(R.id.btn_divide)
     }
 }
-
